802cc9c54ffafd86874483f389ec5b09b750963a
Emmanuel Ajuelos <60262354+ajuelosemmanuel@users.noreply.github.com>
2023-07-05 20:20:23 +0200

    Update payload.ps1

--- Ateliers/RubberDucky/payload.ps1
+++ Ateliers/RubberDucky/payload.ps1
@@ -1,4 +1,4 @@
-$url = "https://cadenaser00.epimg.net/ser/imagenes/2017/09/08/gente/1504852962_952336_1504855721_noticia_normal.jpg"
+$url = "https://raw.githubusercontent.com/ajuelosemmanuel/Ressources_Colos/main/Ateliers/RubberDucky/okcomputer.png"
 $output = "C:\Users\$env:USERNAME\Desktop\bigup.jpg"
 (New-Object System.Net.WebClient).DownloadFile($url, $output)
 $setwallpapersrc = @"
